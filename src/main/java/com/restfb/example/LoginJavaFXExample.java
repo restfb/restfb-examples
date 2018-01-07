67806b00fdc486768fa69d0a224c375ca9e9b372
Norbert Bartels <n.bartels@phpmonkeys.de>
2018-01-07 17:13:37 +0100

    Issue #7 - copyright changed to 2018

--- src/main/java/com/restfb/example/LoginJavaFXExample.java
+++ src/main/java/com/restfb/example/LoginJavaFXExample.java
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2017 Mark Allen, Norbert Bartels.
+ * Copyright (c) 2018 Mark Allen, Norbert Bartels.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
